51c3ce53106ecdf0ae656ef1f35ca659a4f4894b
schnell <christopher.schnell@hotmail.com>
2024-12-10 00:13:09 +0100

    view, comp and style for /pedigree-chart

--- frontend/src/views/PedigreeChart.js
+++ frontend/src/views/PedigreeChart.js
@@ -1,16 +1,29 @@
 import React from "react";
-import LogoutButton from "../components/LogoutButton";
+import Menu from "../components/Menu";
 import Profile from "../components/Profile";
+import Search from "../components/Search";
+import MyOwnLitterBox from "../components/MyOwnLitterBox";
 
 // style
 import "../style/PedigreeChart.css"
 
 const PedigreeChart = () => {
     return (
-        <div className="pedigree-chart">
-            <h1>Your Pedigree Chart</h1>
-            <Profile />
-            <LogoutButton />
+        <div className="container">
+            <div className="pedigree-chart">
+                <div className="left-column">
+                    <Menu />
+                </div>
+
+                <div className="middle-column">
+                    <Profile />
+                    <MyOwnLitterBox />
+                </div>
+
+                <div className="right-column">
+                    <Search />
+                </div>
+            </div>
         </div>
     );
 };

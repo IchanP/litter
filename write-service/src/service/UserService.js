d1d1897b9d51fde3700690b5ffdac040df0e0a9e
Pontus Grandin <pontusgrandin6@gmail.com>
2025-01-03 20:47:12 +0100

    add validator function

--- write-service/src/service/UserService.js
+++ write-service/src/service/UserService.js
@@ -1,6 +1,16 @@
+import { validateNotUndefined } from '../util/validate'
+
 /**
  * Manages interactions between repositories concerning Users.
  */
 export class UserService {
-
+  /**
+   * Handles the registration of a user by contacting relevant repositories and sending out kafka messages to other services.
+   *
+   * @param {object} registrationData - Object containing the user data.
+   */
+  async registerUser (registrationData) {
+    validateNotUndefined(registrationData.email)
+    validateNotUndefined(registrationData.username)
+  }
 }
---
+++ write-service/src/util/validate.js
@@ -0,0 +1,11 @@
+/**
+ * Validates that the passed variable is not undefined or null and throws an error if it is.
+ *
+ * @param {any} variable - May be any type
+ * @param {string} eType - Variable or object name which cannot be undefined.
+ */
+export function validateNotUndefined (variable, eType) {
+  if (variable === undefined || null) {
+    throw new Error(`${variable} is undefined or null.`)
+  }
+}

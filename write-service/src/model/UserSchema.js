0e94355e1fb15c7a7c7e937adf288c3a0463caa9
Pontus Grandin <pontusgrandin6@gmail.com>
2025-01-03 17:38:21 +0100

    counter for userids and auditable trail/version control

--- write-service/src/model/UserSchema.js
+++ write-service/src/model/UserSchema.js
@@ -1,5 +1,6 @@
 import { model, Schema } from 'mongoose'
 import { BASE_SCHEMA } from './BaseSchema'
+import { Counter } from './Counter.'
 import validator from 'validator'
 
 const userSchema = new Schema({
@@ -8,7 +9,7 @@ const userSchema = new Schema({
     required: [true, 'Email address is required'],
     unique: true,
     lowercase: true,
-    maxLength: 254,
+    maxLength: 254, // NOTE - should this be lower?
     trim: true,
     validate: {
       /**
@@ -28,7 +29,42 @@ const userSchema = new Schema({
        */
       message: props => `${props.value} is not a valid email address!`
     }
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    lowercase: true,
+    minLength: [3, 'The username must be a minimum length of 3 characters.'],
+    maxLength: [40, 'The username must be a maximum length of 40 characters.'],
+    trim: true
+  },
+  events: [{
+    type: String,
+    payload: {
+      oldValue: String,
+      newValue: String
+    },
+    timestamp: Date
+  }],
+  version: {
+    type: Number,
+    default: 0
   }
+}, {})
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew) { // Check if the document is new
+    const count = await Counter.findByIdAndUpdate(
+      { _id: 'userId' },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    )
+    this.userId = count.seq
+  }
+  next()
 })
 
 userSchema.addb(BASE_SCHEMA)
+
+export const UserModel = model('User', userSchema)

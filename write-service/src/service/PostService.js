137c29ea0b2ea3516ffa1184ac998b184ad25aa3
Pontus Grandin <pontusgrandin6@gmail.com>
2025-01-07 05:57:05 +0100

    send and cleanup posts on kafka failure

--- write-service/src/service/PostService.js
+++ write-service/src/service/PostService.js
@@ -2,6 +2,8 @@ import { logger } from '../config/winston-logger.js'
 import { PostRepository } from '../repositories/PostRepository.js'
 import { UserRepository } from '../repositories/UserRepository.js'
 import { BadDataError } from '../util/Errors/BadDataError.js'
+import { KafkaDeliveryError } from '../util/Errors/KafkaDeliveryError.js'
+import { convertMongoCreateAtToISOdate } from '../util/index.js'
 import { validateNotUndefined } from '../util/validate.js'
 import { MessageBroker } from './MessageBroker.js'
 
@@ -26,8 +28,10 @@ export class PostService {
    * Orchestrates the creation of a post object in mongodb and calls the message broker to alert other services of the creation.
    *
    * @param {object} postData - An object with the fields authorId and content.
+   * @returns {object} - Returns an object containing the created post.
    */
   async createPost (postData) {
+    let createdPost
     try {
       console.log(postData)
       this.#performPostValidation(postData)
@@ -36,10 +40,21 @@ export class PostService {
       if (!foundUser) {
         throw new BadDataError('No user with that id.')
       }
-      const createdPost = await this.postRepo.createDocument(postData)
+      createdPost = await this.postRepo.createDocument(postData)
+      createdPost.createdAt = convertMongoCreateAtToISOdate(createdPost.createdAt)
+      // TODO might need to pass more data here depending on how we make the post models look.
       await this.broker.sendMessage(process.env.NEW_POST_TOPIC, JSON.stringify(createdPost))
+      return createdPost
     } catch (e) {
-      // TODO handle
+      if (e instanceof KafkaDeliveryError && postData) {
+        try {
+          await this.postRepo.deleteOneRecord({ postId: createdPost?.postId })
+          logger.info('Successfully cleaned up Post creation...')
+        } catch (e) {
+          logger.error('Failed to cleanup Post creation...')
+        }
+        throw e
+      }
       logger.error(`Error on creating User Post: ${e.message}`)
       throw e
     }

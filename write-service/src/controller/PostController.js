6eca746b8be5d59af1e65d9371d39bca3a8d805f
Pontus Grandin <pontusgrandin6@gmail.com>
2025-01-07 04:37:44 +0100

    add a toomuchdataerror class for 409 response

--- write-service/src/controller/PostController.js
+++ write-service/src/controller/PostController.js
@@ -1,4 +1,6 @@
 import { PostService } from '../service/PostService.js'
+import createError from 'http-errors'
+
 /**
  * Controller for managing the post relevant operations for MongoDB.
  */
@@ -21,6 +23,29 @@ export class PostController {
    * @returns {Response} - Returns an express response object with status 201 containign the created user data.
    */
   async createPost (req, res, next) {
+    try {
+      const body = req.body
+    } catch (e) {
+      this.#handleError(e, next)
+    }
+  }
 
+  /**
+   * Returns correct error codes that may occur during the editing or creation of user records.
+   *
+   * @param {Error} e - The error that occured
+   * @param {Function} next - The next middleware function.
+   */
+  #handleError (e, next) {
+    const err = e
+    // TODO - set this up for correct routes
+    /* if (e instanceof BadDataError) {
+      err = createError(400, e.message)
+    } else if (e instanceof DuplicateError) {
+      err = createError(409, e.message)
+    } else if (e instanceof KafkaDeliveryError) {
+      err = createError(500)
+    } */
+    next(err)
   }
 }

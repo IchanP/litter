71ddc6c50a900b81c8b84aa4dd3af474cb21e854
Pontus Grandin <pontusgrandin6@gmail.com>
2025-01-09 09:09:36 +0100

    add auto horizontal scaling for all services

--- user-service/src/config/mongoose.js
+++ user-service/src/config/mongoose.js
@@ -7,7 +7,7 @@
 
 import mongoose from 'mongoose'
 import { logger } from './winston-logger.js'
-import { UserModel } from '../model/UserSchema.js'
+import { UserModel } from '../model/userSchema.js'
 
 /**
  * Establishes a connection to a database.

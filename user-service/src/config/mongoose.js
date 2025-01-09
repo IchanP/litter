72037b73ea8dd654d4c7daa13e3ee55d10ec5be2
Pontus Grandin <pontusgrandin6@gmail.com>
2025-01-09 04:39:42 +0100

    linting

--- user-service/src/config/mongoose.js
+++ user-service/src/config/mongoose.js
@@ -1,8 +1,8 @@
 /**
  * @file This module contains the configuration for the Mongoose ODM.
  * @module config/mongoose
- * @author Mats Loock
- * @version 3.1.0
+ * @author Mats Loock, Pontus Grandin
+ * @version 3.3.0
  */
 
 import mongoose from 'mongoose'
@@ -16,35 +16,42 @@ import { UserModel } from '../model/UserSchema.js'
  * @returns {Promise<mongoose.Mongoose>} Resolves to a Mongoose instance if connection succeeded.
  */
 export const connectToDatabase = async (connectionString) => {
-    const { connection } = mongoose
+  const { connection } = mongoose
 
-    // Will cause errors to be produced instead of dropping the bad data.
-    mongoose.set('strict', 'throw')
+  // Will cause errors to be produced instead of dropping the bad data.
+  mongoose.set('strict', 'throw')
 
-    // Turn on strict mode for query filters.
-    mongoose.set('strictQuery', true)
+  // Turn on strict mode for query filt/*  */ers.
+  mongoose.set('strictQuery', true)
 
-    logger.info('Syncing indexes...')
-    UserModel.syncIndexes()
-    PostModel.syncIndexes()
+  logger.info('Syncing indexes...')
+  UserModel.syncIndexes()
 
-    // Bind connection to events (to get notifications).
-    connection.on('connected', () => logger.info('Mongoose connected to MongoDB.'))
-    connection.on('error', (err) => logger.info(`Mongoose connection error: ${err}`))
-    connection.on('disconnected', () => logger.info('Mongoose disconnected from MongoDB.'))
+  // Bind connection to events (to get notifications).
+  connection.on('connected', () =>
+    logger.info('Mongoose connected to MongoDB.')
+  )
+  connection.on('error', (err) =>
+    logger.info(`Mongoose connection error: ${err}`)
+  )
+  connection.on('disconnected', () =>
+    logger.info('Mongoose disconnected from MongoDB.')
+  )
 
-    // If the Node.js process ends, close the connection.
-    for (const signalEvent of ['SIGINT', 'SIGTERM']) {
-        process.on(signalEvent, () => {
-        (async () => {
-            await connection.close()
-            logger.info(`Mongoose disconnected from MongoDB through ${signalEvent}.`)
-            process.exit(0)
-        })()
-        })
-    }
+  // If the Node.js process ends, close the connection.
+  for (const signalEvent of ['SIGINT', 'SIGTERM']) {
+    process.on(signalEvent, () => {
+      (async () => {
+        await connection.close()
+        logger.info(
+          `Mongoose disconnected from MongoDB through ${signalEvent}.`
+        )
+        process.exit(0)
+      })()
+    })
+  }
 
-    // Connect to the server.
-    logger.info('Mongoose connecting to MongoDB.')
-    return mongoose.connect(connectionString)
+  // Connect to the server.
+  logger.info('Mongoose connecting to MongoDB.')
+  return mongoose.connect(connectionString)
 }

627e8c69db1accce429aa69cf7bf2e33a54322fa
schnell <christopher.schnell@hotmail.com>
2025-01-11 00:12:47 +0100

    updated path

--- frontend/src/components/LitterBox.js
+++ frontend/src/components/LitterBox.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import Loading from "./components/Loading";
+import Loading from "./Loading";
 
 
 // style
